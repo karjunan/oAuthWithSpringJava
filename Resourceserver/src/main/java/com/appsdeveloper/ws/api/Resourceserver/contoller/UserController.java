cc4bc0790ddebd6a68eb77f9f8718c26365f2080
krishnakumarArjunan <arjunankrishnakumar1@gmail.com>
2021-03-06 21:06:49 +0530

    API Gateway , load balancer, Discovery service integration

--- Resourceserver/src/main/java/com/appsdeveloper/ws/api/Resourceserver/contoller/UserController.java
+++ Resourceserver/src/main/java/com/appsdeveloper/ws/api/Resourceserver/contoller/UserController.java
@@ -1,6 +1,7 @@
 package com.appsdeveloper.ws.api.Resourceserver.contoller;
 
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,10 +9,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    Environment env;
+
     @GetMapping("/status/check")
     public String status() {
 
-        return "Working";
+        return "Working on port => " + env.getProperty("local.server.port");
     }
 
     @PreAuthorize("hasRole('developer')")
